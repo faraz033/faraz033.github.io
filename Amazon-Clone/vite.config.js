9aeac8d5f247d674b600ee852103aaa83ffc3753
faraz033 <farazfarooqui033@gmail.com>
2026-05-27 15:24:46 +0530

    updated

--- Amazon-Clone/vite.config.js
+++ Amazon-Clone/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+export default {
+  base: '/Amazon-Clone/',
+}
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
